4c3ab7dbc72f61baaddfe142b0acd31127fdb172
MAugspurger <110561026+MAugspurger@users.noreply.github.com>
2022-10-31 13:18:00 -0500

    Add files via upload

--- Notebooks/ModSimPy_Functions/chap06.py
+++ Notebooks/ModSimPy_Functions/chap06.py
@@ -2,7 +2,8 @@ from ModSimPy_Functions.modsim import *
 import pandas as pd
 
 
-def make_system(G0, k1, k2, k3, dt, data):
+def make_system(params, data):
+    G0, k1, k2, k3, dt = params
     t_0 = data.index[0]
     t_end = data.index[-1]
     
@@ -11,14 +12,14 @@ def make_system(G0, k1, k2, k3, dt, data):
     
     I = interp1d(data.insulin.index,data.insulin.values)
 
-    state = pd.Series(dict(G=G0, X=0),dtype=np.float64)
-    system = dict(init=state,
+    init = pd.Series(dict(G=G0, X=0),dtype=np.float64)
+    system = dict(init=init,
                   k1=k1,k2=k2,
                   k3=k3,dt=dt,
                   Gb=Gb, Ib=Ib, I=I,
                   t_0=t_0, t_end=t_end)
     
-    return system, state
+    return system
 
 def slope_func(t, state, system):
     G, X = state
