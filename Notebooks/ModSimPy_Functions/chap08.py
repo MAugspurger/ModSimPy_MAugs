24229699af52997ff35e5b5605ce4b118570909f
MAugspurger <110561026+MAugspurger@users.noreply.github.com>
2022-11-16 10:57:42 -0600

    Add files via upload

--- Notebooks/ModSimPy_Functions/chap08.py
+++ Notebooks/ModSimPy_Functions/chap08.py
@@ -3,9 +3,17 @@ from ModSimPy_Functions.modsim import *
 def drag_force(V, system):
     rho, C_d, area = system['rho'], system['C_d'], system['area']
     
-    mag = rho * vector_mag(V)**2 * C_d * area / 2
-    direction = -vector_hat(V)
-    f_drag = mag * direction
+    # Find the magnitude and direction of the velocity
+    vel_mag = np.sqrt(V.x**2 + V.y**2)
+    dir = V/vel_mag
+
+    # Find the magnitude of the drag force
+    drag_mag = rho * vel_mag**2 * C_d * area * (1/2)
+
+    # Define the direction of the force as opposite that of the  velocity
+    # Notice that "dir" is a vector, so f_drag is vector too
+    f_drag = drag_mag * -dir
+
     return f_drag
 
 
@@ -13,13 +21,15 @@ def slope_func(t, state, system):
     x, y, vx, vy = state
     mass, g = system['mass'], system['g']
     
-    V = Vector(vx, vy)
+    V = pd.Series(dict(x=vx, y=vy),dtype=float)
     a_drag = drag_force(V, system) / mass
-    a_grav = g * Vector(0, -1)
+
+    # Acceleration has to be defined as a vector too
+    a_grav = pd.Series(dict(x=0,y=-g),dtype=float)
     
     A = a_grav + a_drag
     
-    return V.x, V.y, A.x, A.y
+    return V.x, V.y, A.x, A.y  
 
 def event_func(t, state, system):
     x, y, vx, vy = state
